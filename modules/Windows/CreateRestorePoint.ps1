abe7f236b7ff50583d10e43fef02c9177b9294a6
Couleur <82747632+couleurm@users.noreply.github.com>
2022-03-12 23:47:29 +0100

    remove quotes

--- modules/Windows/CreateRestorePoint.ps1
+++ modules/Windows/CreateRestorePoint.ps1
@@ -29,33 +29,3 @@ function CreateRestorePoint
 		Disable-ComputerRestore -Drive $env:SystemDrive
 	}
 }
-function CreateRestorePoint
-{
-	$SystemDriveUniqueID = (Get-Volume | Where-Object -FilterScript {$_.DriveLetter -eq "$($env:SystemDrive[0])"}).UniqueID
-	$SystemProtection = ((Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SPP\Clients")."{09F7EDC5-294E-4180-AF6A-FB0E6A0E9513}") | Where-Object -FilterScript {$_ -match [regex]::Escape($SystemDriveUniqueID)}
-
-	$ComputerRestorePoint = $false
-
-	switch ($null -eq $SystemProtection)
-	{
-		$true
-		{
-			$ComputerRestorePoint = $true
-			Enable-ComputerRestore -Drive $env:SystemDrive
-		}
-	}
-
-	# Never skip creating a restore point
-	New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" -Name SystemRestorePointCreationFrequency -PropertyType DWord -Value 0 -Force
-
-	Checkpoint-Computer -Description "Sophia Script for Windows 10" -RestorePointType MODIFY_SETTINGS
-
-	# Revert the System Restore checkpoint creation frequency to 1440 minutes
-	New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" -Name SystemRestorePointCreationFrequency -PropertyType DWord -Value 1440 -Force
-
-	# Turn off System Protection for the system drive if it was turned off before without deleting the existing restore points
-	if ($ComputerRestorePoint)
-	{
-		Disable-ComputerRestore -Drive $env:SystemDrive
-	}
-}
