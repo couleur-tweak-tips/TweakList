3564b65daf70d4234cde2ef91e608b10e0a53fe0
Couleur <82747632+couleurm@users.noreply.github.com>
2022-03-12 22:36:37 +0100

    use releases & nicer output

--- build.ps1
+++ build.ps1
@@ -1,16 +1,16 @@
-Get-ChildItem -Recurse
 [Array]$Functions = @() # Makes a fresh empty array
 Get-ChildItem modules -Recurse -Include *.ps1 | ForEach-Object {
 
-$Functions += Get-Content $PSItem
+    Write-Output "- $($PSItem.FullName -replace '/home/runner/work/TweakList/TweakList/modules/','')" # Cleans the output of the github actions
+    $Functions += Get-Content $PSItem
 
+}
 
-} # The #region **filename** / #endregion makes 
-
-$API = (Invoke-WebRequest -Useb  "https://api.github.com/repos/couleur-tweak-tips/TweakList/commits?per_page=1").RawContent # Pings GitHub's API
+$API = (Invoke-WebRequest -Useb  "https://api.github.com/repos/couleur-tweak-tips/TweakList/commits?per_page=1").RawContent # Get TweakList repo commit info from GitHub's API
 $CommitCount = (($API -split "&page=")[2] -split '>; rel="last"')[0] # Parses the API response to get the number of commits
+Write-Output "Commit count: $CommitCount"
 Set-Content -Path ./Master.ps1 -Value @"
 # Commit count: $($CommitCount + 1)
-# This file is automatically built at every commit to add up every function to a single file, this makes it simplier to download and execute.
+# This file is automatically built at every commit to add up every function to a single file, this makes it simplier to parse (aka download) and execute.
 "@ -Force
 Add-Content -Path ./Master.ps1 -Value $Functions -Force
