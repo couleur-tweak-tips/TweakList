ea301d8ca35140fd630e0274d1bcc1e477acc52e
Couleur <82747632+couleurm@users.noreply.github.com>
2022-04-21 22:55:00 +0200

    update build script (fancy recap / error handling)

--- build.ps1
+++ build.ps1
@@ -1,16 +1,36 @@
-[Array]$Functions = @() # Makes a fresh empty array
-Get-ChildItem modules -Recurse -Include *.ps1 | ForEach-Object {
+[Array]$Functions = @()
+$FuncsCount = 0
+Get-ChildItem helpers,modules -Recurse -Include *.ps1 | ForEach-Object {
 
     Write-Output "- $($PSItem.FullName -replace '/home/runner/work/TweakList/TweakList/modules','')" # Cleans the output of the github actions
     $Functions += Get-Content $PSItem
+    $FuncsCount++
 
 }
 
 $API = (Invoke-WebRequest -Useb  "https://api.github.com/repos/couleur-tweak-tips/TweakList/commits?per_page=1").RawContent # Get TweakList repo commit info from GitHub's API
-$CommitCount = (($API -split "&page=")[2] -split '>; rel="last"')[0] # Parses the API response to get the number of commits
-Write-Output "Commit count: $CommitCount"
+Try{
+    Set-Variable -Name CommitCount -Value ([int]((($API -split "&page=")[2] -split '>; rel="last"')[0]) + 1) -ErrorAction Stop # Parses the API response to get the number of commits
+} Catch {
+    Write-Warning "Failed to parse commit count"
+    exit 1
+}
+
 Set-Content -Path ./Master.ps1 -Value @"
-# Commit count: $($CommitCount + 1)
 # This file is automatically built at every commit to add up every function to a single file, this makes it simplier to parse (aka download) and execute.
+
+`$CommitCount = $CommitCount
+`$FuncsCount = $FuncsCount
 "@ -Force
+
 Add-Content -Path ./Master.ps1 -Value $Functions -Force
+
+$AutoBuildSize = (Get-Item ./Master.ps1).Length / 1MB
+
+Write-Output @"
+Commit count: $CommitCount
+FuncsCount: $FuncsCount
+Autobuild size: $([Math]::Round($AutoBuildSize, 2))
+Aotubuild lines: $((Get-Content ./Master.ps1).Count)
+"@
+pause
