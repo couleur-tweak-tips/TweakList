22407b7d1c034eaf99bf744c444cf9b9abf2749d
Couleur <82747632+couleurm@users.noreply.github.com>
2022-04-21 22:55:58 +0200

    Update Set-Win32PrioritySeparation.ps1

--- modules/Windows/Set-Win32PrioritySeparation.ps1
+++ modules/Windows/Set-Win32PrioritySeparation.ps1
@@ -1,8 +1,11 @@
-function Set-Win32ProritySeparation ([int]$Value){
+function Set-Win32ProritySeparation ([int]$DWord){
 
     $Path = 'REGISTRY::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\PriorityControl'
     $current = (Get-ItemProperty $Path).Win32PrioritySeparation
-    Set-ItemProperty -Path $Path -Value $Value -Type DWord -Force -ErrorAction Inquire
-    Write-Verbose "Set-Win32ProritySeparation: Changed from $current to $((Get-ItemProperty $Path).Win32PrioritySeparation)"-Verbose
+
+    Set-ItemProperty -Path ($Path).Win32PrioritySeparation -Value $Value -Type DWord -Force -ErrorAction Inquire
+
+    Write-Verbose "Set-Win32ProritySeparation: Changed from $current to $((Get-ItemProperty $Path).Win32PrioritySeparation)"
 
 }
+
