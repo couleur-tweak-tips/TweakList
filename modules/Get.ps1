723f5aa438e98fe0010371f046d0c6a01e6f1ab8
Couleur <82747632+couleurm@users.noreply.github.com>
2022-04-30 12:00:22 +0200

    Get: add ffmpeg check to dc, and add OBS

--- modules/Get.ps1
+++ modules/Get.ps1
@@ -14,6 +14,7 @@ function Get {
         switch ($App){
             #'Voukoder'{} soonTMTM
             'Upscaler'{
+
                 Install-FFmpeg
                 Invoke-RestMethod 'https://github.com/couleur-tweak-tips/utils/raw/main/Miscellaneous/CTT%20Upscaler.cmd' |
                 Out-File (Join-Path ([System.Environment]::GetFolderPath('SendTo')) 'CTT Upscaler.cmd') -Encoding ASCII -Force
@@ -21,17 +22,19 @@ function Get {
 CTT Upscaler has been installed,
 I strongly recommend you open settings to tune it to your PC, there's lots of cool stuff to do there!
 "@ -ForegroundColor Green
+
             }
             'Scoop'{Install-Scoop}
             'FFmpeg'{Install-FFmpeg}
             {$_ -In '7-Zip','7z','7Zip'}{Get-ScoopApp 7zip}
             {$_ -In 'Smoothie','sm'}{Install-FFmpeg;Get-ScoopApp utils/Smoothie}
+            {$_ -In 'OBS','OBStudio'}{Get-ScoopApp extras/obs-studio}
             {$_ -In 'UTVideo'}{Get-ScoopApp utils/utvideo}
             {$_ -In 'Nmkoder'}{Get-ScoopApp utils/nmkoder}
             {$_ -In 'Librewolf'}{Get-ScoopApp extras/librewolf}
             {$_ -In 'ffmpeg-nightly'}{Get-ScoopApp versions/ffmpeg-nightly}
             {$_ -In 'Graal','GraalVM'}{Get-ScoopApp utils/GraalVM}
-            {$_ -In 'DiscordCompressor','dc'}{Get-ScoopApp utils/discordcompressor}
+            {$_ -In 'DiscordCompressor','dc'}{Install-FFmpeg;Get-ScoopApp utils/discordcompressor}
             {$_ -In 'Moony','mn'}{if (-Not(Test-Path "$HOME\.lunarclient")){Write-Warning "You NEED Lunar Client to launch it with Moony"};Get-ScoopApp utils/Moony}
             {$_ -In 'TLShell','TLS'}{Get-TLShell}
             default{Get-ScoopApp $App}
