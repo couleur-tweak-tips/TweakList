5c4b31f3b52081d0972c29c166224a00031fddc7
Couleur <82747632+couleurm@users.noreply.github.com>
2022-05-02 12:17:38 +0200

    arg fixes

--- modules/Installers/Install-Voukoder.ps1
+++ modules/Installers/Install-Voukoder.ps1
@@ -1,9 +1,9 @@
 function Install-Voukoder {
     [alias('isvk')]
     param(
-        [Switch]$GetTemplates = $false
+        [Switch]$GetTemplates = $false # Skip Voukoder installation
     )
-    if ($PSEdition -eq 'Core'){return "This command is only available on Windows PowerShell (use of Get-Package)."}
+    if ($PSEdition -eq 'Core'){return "Install-Voukoder is only available on Windows PowerShell 5.1 (use of Get-Package)."}
     if (!$GetTemplates){
         $LatestCore = (Invoke-RestMethod https://api.github.com/repos/Vouk/voukoder/releases)[0]
         if ($LatestCore.tag_name -notlike "*.*"){
@@ -17,11 +17,12 @@ function Install-Voukoder {
                 "Updating Voukoder Core from version $CurrentVersion to $LatestCoreVersion"
                 Start-Process -FilePath msiexec -ArgumentList "/qb /x {$($Core.TagId)}" -Wait -NoNewWindow
             }
+        }else{
+            "Downloading and installing Voukoder Core.."
+            $CoreURL = $LatestCore[0].assets[0].browser_download_url
+            curl.exe -# -L $CoreURL -o"$env:TMP\Voukoder-Core.msi"
+            msiexec /i "$env:TMP\Voukoder-Core.msi" /passive    
         }
-        "Downloading and running Voukoder Core.."
-        $CoreURL = $LatestCore[0].assets[0].browser_download_url
-        curl.exe -# -L $CoreURL -o"$env:TMP\Voukoder-Core.msi"
-        msiexec /i "$env:TMP\Voukoder-Core.msi" /passive
 
         $Tree = (Invoke-RestMethod 'https://api.github.com/repos/Vouk/voukoder-connectors/git/trees/master?recursive=1').Tree
         
@@ -29,6 +30,7 @@ function Install-Voukoder {
             $Path = $Tree.path | Where-Object {$_ -Like "*$NLE-connector*"} | Sort-Object -Descending | Select-Object -First 1
             $Connectors += @{$NLE = "https://github.com/Vouk/voukoder-connectors/raw/master/$Path"}
         }
+        $NLE = $null
 
         $Processes = @(
             'vegas*'
@@ -44,7 +46,7 @@ function Install-Voukoder {
         function Get-ConnectorVersion ($FileName){
             return $FileName.Trim('.msi').Trim('.zip').Split('-') | Select-Object -Last 1
         }
-        function CheckConnector ($PackageName, $Key){
+        function NeedsConnector ($PackageName, $Key){
             
             $CurrentConnector = (Get-Package -Name $PackageName -ErrorAction Ignore)
             if ($CurrentConnector){
@@ -60,52 +62,55 @@ function Install-Voukoder {
         $NLEs = Get-Process $Processes -ErrorAction Ignore
         ForEach($NLE in $NLEs){
             switch (Split-Path $NLE.Path -Leaf){
-                'vegas180' {
+                {$_ -in 'vegas180.exe', 'vegas190.exe'} {
+                    Write-Verbose "Found VEGAS18+"
 
-                    if (-Not(CheckConnector -PackageName 'Voukoder connector for VEGAS' -Key 'vegas18')){
+                    if (NeedsConnector -PackageName 'Voukoder connector for VEGAS' -Key 'vegas18'){
                         continue
                     }
                     $Directory = Split-Path $NLE.Path -Parent
                     curl.exe -# -L $Connectors.vegas18 -o"$env:TMP\Voukoder-Connector-VEGAS18.msi"
-                    msiexec /i "$env:TMP\Voukoder-Connector-VEGAS18.msi" /qn "VEGASDIR=$Directory"
+                    msiexec /i "C:\Users\Dek\AppData\Local\Temp\Voukoder-Connector-VEGAS18.msi" /qb "VEGASDIR=`"$Directory`""
+                    continue
                 }
-                {$_ -Like 'vegas*'}{
-                    if (-Not(CheckConnector -PackageName 'Voukoder connector for VEGAS' -Key 'vegas')){
+                {$_ -Like 'vegas*.exe'}{
+                    Write-Verbose "Found old VEGAS"
+                    if (NeedsConnector -PackageName 'Voukoder connector for VEGAS' -Key 'vegas'){
                         continue
                     }
                     $Directory = Split-Path $NLE.Path -Parent
                     curl.exe -# -L $Connectors.vegas18 -o"$env:TMP\Voukoder-Connector-VEGAS.msi"
-                    msiexec /i "$env:TMP\Voukoder-Connector-VEGAS.msi" /qn "VEGASDIR=$Directory"
+                    msiexec /i "$env:TMP\Voukoder-Connector-VEGAS.msi" /qb "VEGASDIR=$Directory"
+                    continue
                 }
-                'aftereffects' {
-                    if (-Not(CheckConnector -PackageName 'Voukoder connector for After Effects' -Key 'aftereffects')){
+                'afterfx.exe' {
+                    if (NeedsConnector -PackageName 'Voukoder connector for After Effects' -Key 'aftereffects'){
                         continue
                     }
                     $Directory = Split-Path $NLE.Path -Parent
                     curl.exe -# -L $Connectors.aftereffects -o"$env:TMP\AE.msi"
-                    msiexec /i "$env:TMP\Voukoder-Connector-AE.msi" /qn "INSTALLDIR=C:\Program Files\Adobe\Common\Plug-ins\7.0\MediaCore"
+                    msiexec /i "$env:TMP\Voukoder-Connector-AE.msi" /qb "INSTALLDIR=C:\Program Files\Adobe\Common\Plug-ins\7.0\MediaCore"
                 }
-                'Adobe Premiere Pro'{
-                    if (-Not(CheckConnector -PackageName 'Voukoder connector for Premiere Pro' -Key 'premiere')){
+                'Adobe Premiere Pro.exe'{
+                    if (NeedsConnector -PackageName 'Voukoder connector for Premiere Pro' -Key 'premiere'){
                         continue
                     }
                     $Directory = Split-Path $NLE.Path -Parent
                     curl.exe -# -L $Connectors.premiere -o"$env:TMP\Voukoder-Connector-Premiere.msi"
-                    msiexec /i "$env:TMP\Voukoder-Connector-Premiere.msi" /qn "TGDir=C:\Program Files\Adobe\Common\Plug-ins\7.0\MediaCore"
+                    msiexec /i "$env:TMP\Voukoder-Connector-Premiere.msi" /qb "TGDir=C:\Program Files\Adobe\Common\Plug-ins\7.0\MediaCore"
                 }
                 'Resolve'{
                     $IOPlugins = "$env:ProgramData\Blackmagic Design\DaVinci Resolve\Support\IOPlugins"
                     New-Item -ItemType Directory -Path $IOPlugins -ErrorAction Ignore | Out-Null
                     if (Test-Path "$IOPlugins\voukoder_plugin.dvcp.bundle"){
-                    if (-Not(Get-Boolean "Would you like to reinstall/update the Voukoder Resolve plugin? (Y/N)")){continue}
-                    Remove-Item "$IOPlugins\voukoder_plugin.dvcp.bundle" -Force -Recurse
+                        if (-Not(Get-Boolean "Would you like to reinstall/update the Voukoder Resolve plugin? (Y/N)")){continue}
+                        Remove-Item "$IOPlugins\voukoder_plugin.dvcp.bundle" -Force -Recurse
                     }
                     curl.exe -# -L $Connectors.Resolve -o"$env:TMP\Voukoder-Connector-Resolve.zip"
                     Remove-Item "$env:TMP\Voukoder-Connector-Resolve" -Recurse -Force -ErrorAction Ignore
                     $ExtractDir = "$env:TMP\Voukoder-Connector-Resolve"
                     Expand-Archive "$env:TMP\Voukoder-Connector-Resolve.zip" -Destination $ExtractDir
                     Copy-Item "$ExtractDir\resolve-connector-*\voukoder_plugin.dvcp.bundle" $IOPlugins
-
                 }
             }
         }
@@ -121,7 +126,6 @@ function Install-Voukoder {
         'libx265' =              'https://cdn.discordapp.com/attachments/969870701798522901/969872700958965780/libx265.sft2'
     }
 
-
     $SelectedTemplates = Write-Menu -Entries @($Templates.Keys) -MultiSelect -Title @"
 Tick/untick the render templates you'd like to install by pressing SPACE, then press ENTER to finish.
 NVENC (for NVIDIA GPUs) is much faster than libx265, but will give you a bigger file to upload.
@@ -130,5 +134,5 @@ NVENC (for NVIDIA GPUs) is much faster than libx265, but will give you a bigger 
         Remove-Item "$TemplatesFolder\$Template.sft2" -Force -ErrorAction Ignore
         curl.exe -# -sSL $Templates.$Template -o"$TemplatesFolder\$Template.sft2"
     }
+    Write-Output "Installation script finished, restart your NLE to see the new render templates."
 }
-
