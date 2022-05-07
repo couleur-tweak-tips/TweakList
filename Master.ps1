71d20e437f4a71e4e2ea1ed493fe2fc0613c77f9
couleurm <couleurm@users.noreply.github.com>
2022-05-07 17:09:10 +0000

    Autobuild

--- Master.ps1
+++ Master.ps1
@@ -1,7 +1,7 @@
 # This file is automatically built at every commit to add up every function to a single file, this makes it simplier to parse (aka download) and execute.
 
-$CommitCount = 104
-$FuncsCount = 43
+$CommitCount = 110
+$FuncsCount = 45
 <#
 The MIT License (MIT)
 
@@ -755,7 +755,7 @@ function Install-FFmpeg {
 
     if(Get-Command ffmpeg -Ea Ignore){
 
-        $IsFFmpeg5 = (ffmpeg -hide_banner -h filter=libplacebo)
+        $IsFFmpeg5 = (ffmpeg -hide_banner -h filter=libplacebo) -ne "Unknown filter 'libplacebo'."
 
         if (-Not($IsFFmpeg5)){
 
@@ -763,10 +763,11 @@ function Install-FFmpeg {
                 scoop update ffmpeg
             }else{
                 Write-Warning @"
-An FFmpeg installation was detected, but it is not version 5.0 or higher.
+An FFmpeg installation was detected, but libplacebo filter could not be found (old FFmpeg version?).
 If you installed FFmpeg yourself, you can remove it and use the following command to install ffmpeg and add it to the path:
-scoop install ffmpeg
+scoop.cmd install ffmpeg
 "@
+pause
                 
             }
             
@@ -1917,27 +1918,31 @@ function Optimize-OptiFine {
         [ValidateSet('Smart','Lowest')]
         [Parameter(Mandatory)]
         $Preset,
-        [String]$CustomDirectory,
+        [String]$CustomDirectory = (Join-path $env:APPDATA '.minecraft'),
         [Switch]$MultiMC,
         [Switch]$PolyMC,
         [Switch]$GDLauncher
     )
 
-if (!$CustomDirectory){$CustomDirectory = Join-path $env:APPDATA '.minecraft'}
-elseif($MultiMC){
+if($MultiMC){
     $CustomDirectory = Get-ChildItem "$env:APPDATA\Microsoft\Windows\Start Menu\Programs" -Recurse | Where-Object Name -Like "MultiMC.lnk"
     $CustomDirectory = Get-ShortcutPath $CustomDirectory
-    $CustomDirectory = Join-Path (Split-Path $CustomDirectory) instances
+    $Instances = Get-ChildItem (Join-Path (Split-Path $CustomDirectory) instances)
     "Please select a MultiMC instance"
-    $CustomDirectory = menu (Get-ChildItem $CustomDirectory).Name
+    $CustomDirectory = menu (Get-ChildItem $Instances).Name
+    $CustomDirectory = Join-Path $Instances $CustomDirectory
+
 }elseif($PolyMC){
-    $CustomDirectory = Get-ChildItem "$envAppData\PolyMC\instances"
+    $Instances = Get-ChildItem "$env:APPDATA\PolyMC\instances"
     "Please select a PolyMC instance"
-    $CustomDirectory = $CustomDirectory.Name
+    $CustomDirectory = menu $Instances.Name
+    $CustomDirectory = Join-Path $Instances $CustomDirectory
+
 }elseif($GDLauncher){
-    $CustomDirectory = Get-ChildItem "$envAppData\gdlauncher_next\instances"
+    $Instances = Get-ChildItem "$env:APPDATA\gdlauncher_next\instances"
     "Please select a GDLauncher instance"
-    $CustomDirectory = $CustomDirectory.Name
+    $CustomDirectory = menu $Instances.Name
+    $CustomDirectory = Join-Path $Instances $CustomDirectory
 
 }
 
@@ -1965,6 +1970,7 @@ $Presets = @{
             showInventoryAchievementHint=$false
         }
         optionsof = @{
+            ofAaLevel=0 # Anti-Aliasing
             ofDynamicLights=3
             ofChunkUpdates=1
             ofAoLevel=0.0 # Smooth lighting
@@ -2083,7 +2089,7 @@ function Install-Voukoder {
             'Resolve'
         )
         Write-Host "Looking for $($Processes -Join ', ').."
-        
+
         While(!(Get-Process $Processes -ErrorAction Ignore)){
             Write-Host "`rScanning for any opened NLEs (video editors), press any key to refresh.." -NoNewline -ForeGroundColor Green
             Start-Sleep -Seconds 2
@@ -2143,7 +2149,7 @@ function Install-Voukoder {
                     }
                     $Directory = Split-Path $NLE.Path -Parent
                     curl.exe -# -L $Connectors.premiere -o"$env:TMP\Voukoder-Connector-Premiere.msi"
-                    msiexec /i "$env:TMP\Voukoder-Connector-Premiere.msi" /qb "TGDir=`"$env:ProgramFiles\Adobe\Common\Plug-ins\7.0\MediaCore`""
+                    msiexec /i "$env:TMP\Voukoder-Connector-Premiere.msi" /qb "TGTDir=`"$env:ProgramFiles\Adobe\Common\Plug-ins\7.0\MediaCore`""
                 }
                 'Resolve'{
                     $IOPlugins = "$env:ProgramData\Blackmagic Design\DaVinci Resolve\Support\IOPlugins"
@@ -2166,10 +2172,14 @@ function Install-Voukoder {
     New-Item -ItemType Directory -Path "$env:APPDATA\VEGAS\Render Templates\voukoder" -Force -ErrorAction Ignore | Out-Null
 
     $Templates = [Ordered]@{
-        'HEVC NVENC + Upscale' = 'https://cdn.discordapp.com/attachments/969870701798522901/969870704520613889/HEVC_NVENC__upscale.sft2'
-        'HEVC NVENC' =           'https://cdn.discordapp.com/attachments/969870701798522901/969871122491400252/HEVC_NVENC.sft2'
-        'libx265 + Upscale' =    'https://cdn.discordapp.com/attachments/969870701798522901/969872715974598706/libx265__upscale.sft2'
-        'libx265' =              'https://cdn.discordapp.com/attachments/969870701798522901/969872700958965780/libx265.sft2'
+        'HEVC NVENC + Upscale' = 'https://cdn.discordapp.com/attachments/969870701798522901/972541638578667540/HEVC_NVENC_Upscale.sft2'
+        'HEVC NVENC' =           'https://cdn.discordapp.com/attachments/969870701798522901/972541638733885470/HEVC_NVENC.sft2'
+        'H264 NVENC + Upscale' = 'https://cdn.discordapp.com/attachments/969870701798522901/972541639744688198/H264_NVENC_Upscale.sft2'
+        'H264 NVENC' =           'https://cdn.discordapp.com/attachments/969870701798522901/972541638356389918/H264_NVENC.sft2'
+        'x265 + Upscale' =       'https://cdn.discordapp.com/attachments/969870701798522901/972541639346225264/x265_Upscale.sft2'
+        'x265' =                 'https://cdn.discordapp.com/attachments/969870701798522901/972541639560163348/x265.sft2'
+        'x264 + Upscale' =       'https://cdn.discordapp.com/attachments/969870701798522901/972541638943596574/x264_Upscale.sft2'
+        'x264' =                 'https://cdn.discordapp.com/attachments/969870701798522901/972541639128129576/x264.sft2'
     }
 
     $SelectedTemplates = Write-Menu -Entries @($Templates.Keys) -MultiSelect -Title @"
@@ -2294,6 +2304,44 @@ function Set-Win32ProritySeparation ([int]$DWord){
 
 }
 
+function Add-ToContextMenu {
+    param(
+        [ValidateSet(
+            'SendTo',
+            'TakeOwnership',
+            'OpenWithOnBatchFiles'
+            )]
+        [Array]$Entries
+    )
+
+    if ('SendTo' -in $Entries){
+        New-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\AllFilesystemObjects\shellex\ContextMenuHandlers\SendTo -Name "(default)" -PropertyType String -Value "{7BA4C740-9E81-11CF-99D3-00AA004AE837}" -Force
+    }
+
+    if ('OpenWithOnBatchFiles' -in $Entries){
+        New-Item -Path "Registry::HKEY_CLASSES_ROOT\batfile\shell\Open with\command" -Force
+        Set-ItemProperty "Registry::HKEY_CLASSES_ROOT\batfile\shell\Open with\command" -Name "(Default)" -Value "{09799AFB-AD67-11d1-ABCD-00C04FC30936}" -Force
+
+    }
+
+    if ('TakeOwnerShip' -in $Entries){
+        '*','Directory' | ForEach-Object {
+            New-Item -Path "Registry::HKEY_CLASSES_ROOT\$_\shell\runas"
+            New-ItemProperty -LiteralPath "Registry::HKEY_CLASSES_ROOT\$_\shell\runas" -Name '(Default)' -Value 'Take Ownership'
+            New-ItemProperty -LiteralPath "Registry::HKEY_CLASSES_ROOT\$_\shell\runas" -Name 'NoWorkingDirectory' -Value ''
+            New-ItemProperty -LiteralPath "Registry::HKEY_CLASSES_ROOT\$_\shell\runas" -Name 'HasLUAShield' -Value ''
+            New-ItemProperty -LiteralPath "Registry::HKEY_CLASSES_ROOT\$_\shell\runas" -Name 'Position' -Value 'Middle'
+            New-ItemProperty -LiteralPath "Registry::HKEY_CLASSES_ROOT\$_\shell\runas" -Name 'AppliesTo' -Value "NOT (System.ItemPathDisplay:=`"$env:HOMEDRIVE\`")"
+
+            New-Item -Path "Registry::HKEY_CLASSES_ROOT\$_\shell\runas\command"
+            $Command = 'cmd.exe /c title Taking ownership.. & mode con:lines=30 cols=150 & takeown /f "%1" && icacls "%1" /grant administrators:F & timeout 2 >nul'
+            New-ItemProperty -LiteralPath "Registry::HKEY_CLASSES_ROOT\$_\shell\runas\command" -Name '(Default)' -Value $Command
+            New-ItemProperty -LiteralPath "Registry::HKEY_CLASSES_ROOT\$_\shell\runas\command" -Name 'IsolatedCommand' -Value $Command
+
+        }
+    }
+
+}
 function Block-RazerSynapse {
     Try {
         Remove-Item "C:\Windows\Installer\Razer" -Force -Recurse
@@ -2535,6 +2583,7 @@ function Get-NVIDIADriver {
 function Get {
     [alias('g')] # minimalism at it's finest
     param(
+        [Parameter(ValueFromRemainingArguments = $true)]
         [Array]$Apps
     )
 
@@ -2543,7 +2592,7 @@ function Get {
     ForEach($App in $Apps){ # Scoop exits when it throws
 
         switch ($App){
-            #'Voukoder'{} soonTMTM
+            'Voukoder'{Install-Voukoder}
             'Upscaler'{
 
                 Install-FFmpeg
@@ -2555,8 +2604,24 @@ I strongly recommend you open settings to tune it to your PC, there's lots of co
 "@ -ForegroundColor Green
 
             }
+            {$_ -In 'QualityMuncher','qm'}{
+                Install-FFmpeg
+
+                Invoke-RestMethod 'https://raw.githubusercontent.com/Thqrn/qualitymuncher/main/Quality%20Muncher.bat' |
+                Out-File (Join-Path ([System.Environment]::GetFolderPath('SendTo')) 'Quality Muncher.bat') -Encoding ASCII -Force
+
+                Invoke-RestMethod 'https://raw.githubusercontent.com/Thqrn/qualitymuncher/main/!!qualitymuncher%20multiqueue.bat' |
+                Out-File (Join-Path ([System.Environment]::GetFolderPath('SendTo')) '!!qualitymuncher multiqueue.bat') -Encoding ASCII -Force
+
+            }
+
             'Scoop'{Install-Scoop}
             'FFmpeg'{Install-FFmpeg}
+            {$_ -in 'CRU','custom-resolution-utility'}{Get-ScoopApp extras/cru}
+            {$_ -in 'Notepad++','notepadplusplus'}{Get-ScoopApp extras/notepadplusplus}
+            {$_ -in 'DDU','DisplayDriverUninstaller'}{Get-ScoopApp extras/ddu}
+            {$_ -in 'Afterburner','MSIAfterburner'}{Get-ScoopApp utils/msiafterburner}
+            {$_ -in 'Everything','Everything-Alpha','Everything-Beta'}{Get-ScoopApp extras/everything-alpha}
             {$_ -In '7-Zip','7z','7Zip'}{Get-ScoopApp 7zip}
             {$_ -In 'Smoothie','sm'}{Install-FFmpeg;Get-ScoopApp utils/Smoothie}
             {$_ -In 'OBS','OBStudio'}{Get-ScoopApp extras/obs-studio}
@@ -2570,6 +2635,7 @@ I strongly recommend you open settings to tune it to your PC, there's lots of co
             {$_ -In 'TLShell','TLS'}{Get-TLShell}
             default{Get-ScoopApp $App}
         }
+        Write-Verbose "Finished installing $app"
 
     }
     if ($FailedToInstall){
@@ -2634,6 +2700,170 @@ function Import-Sophia {
         Invoke-Expression $SophiaFunctions
     }
 
+}
+function Remove-ContextMenu {
+    [alias('rcm')]
+    <#
+    https://www.tenforums.com
+    https://winaero.com
+    https://majorgeeks.com
+    https://github.com/farag2/Sophia-Script-for-Windows
+    #>
+    param(
+        [ValidateSet(
+            'PinToQuickAccess',
+            'RestorePreviousVersions',
+            'Print',
+            'GiveAccessTo',
+            'EditWithPaint3D',
+            'IncludeInLibrary',
+            'AddToWindowsMediaPlayerList',
+            'CastToDevice',
+            'EditWithPaint3D',
+            'EditWithPhotos',
+            'Share',
+            'TakeOwnerShip',
+            '7Zip',
+            'WinRAR',
+            'Notepad++',
+            'OpenWithOnBatchFiles',
+            'SendTo'
+            )]
+        [Array]$Entries
+    )
+
+    $CurrentPreference = $ErrorActionPreference
+    $ErrorActionPreference = 'Ignore'
+    $Blocked = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked"
+
+    if (-Not (Test-Path -Path $Blocked)){
+        New-Item -Path $Blocked -Force
+    }
+
+    if ('RestorePreviousVersions' -in $Entries){
+        New-ItemProperty -Path "$Blocked" -Name "{596AB062-B4D2-4215-9F74-E9109B0A8153}"
+    }
+
+    if ('PinToQuickAccess'){
+        @('HKEY_CLASSES_ROOT','HKEY_LOCAL_MACHINE\SOFTWARE\Classes') |
+        ForEach-Object { Remove-Item "Registry::$_\Folder\shell\pintohome" -Force -Recurse}
+    }
+
+    if ('Print' -in $Entries){
+        @(
+            'SystemFileAssociations\image',
+            'batfile','cmdfile','docxfil','fonfile','htmlfil','inffile','inifile','VBSFile','WSFFile',
+            'JSEFile','otffile','pfmfile','regfile','rtffile','ttcfile','ttffile','txtfile','VBEFile'
+        ) | ForEach-Object {Set-ItemProperty "Registry::HKEY_CLASSES_ROOT\$_\shell\print" -Name "ProgrammaticAccessOnly" -Value ''}
+    }
+
+    if ('GiveAccessTo' -in $Entries) {
+        @('*','Directory\Background','Directory','Drive','LibraryFolder\background','UserLibraryFolder') |
+        ForEach-Object {Remove-Item -LiteralPath "Registry::HKEY_CLASSES_ROOT\$_\shellex\ContextMenuHandlers\Sharing" -Recurse -Force}
+    }
+
+    if ('IncludeInLibrary' -in $Entries){
+        @('HKEY_LOCAL_MACHINE\SOFTWARE\Classes','HKEY_CLASSES_ROOT') |
+        ForEach-Object {Remove-Item "Registry::$_\Folder\ShellEx\ContextMenuHandlers\Library Location" -Force}
+    }
+
+    if ('AddToWindowsMediaPlayerList' -in $Entries){
+        @(
+            '3G2','3GP','ADTS','AIFF','ASF','ASX','AU','AVI','FLAC','M2TS','m3u','M4A','MIDI','MK3D',
+            'MKA','MKV','MOV','MP3','MP4','MPEG','TTS','WAV','WAX','WMA','WMV','WPL','WVX'
+        ) | ForEach-Object { Remove-Item "Registry::HKEY_CLASSES_ROOT\WMP11.AssocFile.$_\shell\Enqueue" -Force -Recurse }
+
+        @(
+            'MediaCenter.WTVFile','Stack.Audio','Stack.Image','SystemFileAssociations\audio','WMP.WTVFile',
+            'SystemFileAssociations\Directory.Audio','SystemFileAssociations\Directory.Image','WMP.DVR-MSFile','WMP.DVRMSFile'
+        ) | ForEach-Object { Remove-Item "Registry::HKEY_CLASSES_ROOT\$_\shell\Enqueue" -Force -Recurse}
+    }
+
+    if ('CastToDevice' -in $Entries){
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked" -Name "{7AD84985-87B4-4a16-BE58-8B72A5B390F7}" -PropertyType String -Value "Play to menu" -Force
+    }
+
+    if ('EditWithPaint3D' -in $Entries){
+        @('.3mf','.bmp','.fbx','.gif','.jfif','.jpe','.jpeg','.jpg','.png','.tif','.tiff') | 
+        ForEach-Object { Remove-Item "Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\$_\Shell\3D Edit" -Force -Recurse}
+    }
+
+    if ('EditWithPhotos' -in $Entries){
+        Set-ItemProperty "Registry::HKEY_CLASSES_ROOT\AppX43hnxtbyyps62jhe9sqpdzxn1790zetc\Shell\ShellEdit" -Name 'ProgrammaticAccessOnly' -Value ''
+    }
+
+    if ('Share' -in $Entries){
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked" -Name "{E2BF9676-5F8F-435C-97EB-11607A5BEDF7}" -PropertyType String -Value "" -Force
+    }
+
+    if ('TakeOwnerShip' -in $Entries){
+        @(
+        'HKEY_CLASSES_ROOT\*\shell\runas'
+        'HKEY_CLASSES_ROOT\Directory\shell\runas'
+        'HKEY_CLASSES_ROOT\*\shell\TakeOwnership'
+        'HKEY_CLASSES_ROOT\Directory\shell\TakeOwnership'
+        'HKEY_LOCAL_MACHINE\SOFTWARE\Classes\*\shell\TakeOwnership'
+        'HKEY_LOCAL_MACHINE\SOFTWARE\Classes\*\shell\TakeOwnership'
+        'HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Directory\shell\TakeOwnership'
+        ) | ForEach-Object {
+            Remove-Item -LiteralPath "Registry::$_" -Recurse -Force
+        }
+    }
+
+    if ('SendTo' -in $Entries){
+        $DefaultSendTo = (
+        'Bluetooth File Transfer',
+        'Compressed (zipped) Folder',
+        'Desktop (create shortcut)',
+        'Documents',
+        'Fax Recipient',
+        'Mail Recipient'
+        )
+        $NonDefaultSendTo = Get-ChildItem ([System.Environment]::GetFolderPath('SendTo')) | Where-Object BaseName -NotIn $DefaultSendTo
+        if ($NonDefaultSendTo) {
+            $NonDefaultSendTo.Name
+            if(Get-Boolean "Are you sure you wish to lose access the following files/scripts?"){
+                New-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\AllFilesystemObjects\shellex\ContextMenuHandlers\SendTo -Name "(default)" -PropertyType String -Value "-{7BA4C740-9E81-11CF-99D3-00AA004AE837}" -Force
+            }
+        }else{
+            New-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\AllFilesystemObjects\shellex\ContextMenuHandlers\SendTo -Name "(default)" -PropertyType String -Value "-{7BA4C740-9E81-11CF-99D3-00AA004AE837}" -Force
+        }
+    }
+    
+    if ('OpenWithOnBatchFiles' -in $Entries){
+        Remove-Item -Path "Registry::HKEY_CLASSES_ROOT\batfile\shell\Open with\command" -Force -Recurse
+    }
+
+    if ('7Zip' -in $Entries){
+        @(
+            'Classes\CLSID\{23170F69-40C1-278A-1000-000100020000}',
+            'Classes\CLSID\{23170F69-40C1-278A-1000-000100020000}\InprocServer32',
+            'Classes\*\shellex\ContextMenuHandlers\7-Zip',
+            'Classes\Directory\shellex\ContextMenuHandlers\7-Zip',
+            'Classes\Folder\shellex\ContextMenuHandlers\7-Zip',
+            '7-Zip\Options'
+        ) | ForEach-Object {Remove-Item -LiteralPath "REGISTRY::HKEY_CURRENT_USER\Software\$_" -Recurse -Force}
+    }
+    if ('WinRAR' -in $Entries){ # This hides (adds to Blocked) instead of deleting
+        @('{B41DB860-64E4-11D2-9906-E49FADC173CA}','{B41DB860-8EE4-11D2-9906-E49FADC173CA}') |
+        ForEach-Object {New-ItemProperty -Path $Blocked -Name $_ -Value ''}
+    }
+
+    if ('Notepad++' -in $Entries){
+        @(
+            '*\shell\Open with &Notepad++',
+            '*\shell\Open with &Notepad++\command',
+            'Directory\shell\Open with &Notepad++',
+            'Directory\shell\Open with &Notepad++\command',
+            'Directory\Background\shell\Open with &Notepad++',
+            'Directory\Background\shell\Open with &Notepad++\command'
+        ) | ForEach-Object {
+            Remove-Item -LiteralPath "Registry::HKEY_CURRENT_USER\Software\Classes\$_" -Recurse -Force
+        }
+
+    }
+
+    $ErrorActionPreference = $CurrentPreference
 }
 function Remove-FromThisPC {
     param(
