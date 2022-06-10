923d186a04587baa422826ada84cda3293f95ab4
Couleur <82747632+couleurm@users.noreply.github.com>
2022-06-10 11:45:53 +0200

    Get-EncodingArgs: add EzEncArgs option

--- helpers/Get-EncodingArgs.ps1
+++ helpers/Get-EncodingArgs.ps1
@@ -2,37 +2,42 @@
 function Get-EncodingArgs{
     [alias('genca')]
     param(
-        [Switch]$Silent
+        [String]$Resolution = '3840x2160',
+        [Switch]$Silent,
+        [Switch]$EzEncArgs
     )
 
 Install-FFmpeg
 
 $DriverVersion = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8}_Display.Driver" -ErrorAction Ignore).DisplayVersion
-    if ($DriverVersion){ # Only triggers if it parsed a NVIDIA driver version
+    if ($DriverVersion){ # Only triggers if it parsed a NVIDIA driver version, else it can probably be an NVIDIA GPU
         if ($DriverVersion -lt 477.41){ # Oldest NVIDIA version capable
-        Write-Warning "Outdated NVIDIA Drivers detected ($DriverVersion), NVIDIA settings won't be available until you upgrade your drivers"
+        Write-Warning "Outdated NVIDIA Drivers detected ($DriverVersion), you won't be able to encode using NVENC util you update them."
     }
 }
 
-@(
-    'hevc_nvenc -rc constqp -preset p7 -qp 18'
-    'h264_nvenc -rc constqp -preset p7 -qp 15'
-    'hevc_amf -quality quality -qp_i 16 -qp_p 18 -qp_b 20'
-    'h264_amf -quality quality -qp_i 12 -qp_p 12 -qp_b 12'
-    'hevc_qsv -preset veryslow -global_quality:v 18'
-    'h264_qsv -preset veryslow -global_quality:v 15'
-    'libx265 -preset medium -crf 18'
-    'libx264 -preset slow -crf 15'
-) | ForEach-Object -Begin {
+$EncCommands = [ordered]@{
+    'HEVC NVENC' = 'hevc_nvenc -rc constqp -preset p7 -qp 18'
+    'H264 NVENC' = 'h264_nvenc -rc constqp -preset p7 -qp 15'
+    'HEVC AMF' = 'hevc_amf -quality quality -qp_i 16 -qp_p 18 -qp_b 20'
+    'H264 AMF' = 'h264_amf -quality quality -qp_i 12 -qp_p 12 -qp_b 12'
+    'HEVC QSV' = 'hevc_qsv -preset veryslow -global_quality:v 18'
+    'H264 QSV' = 'h264_qsv -preset veryslow -global_quality:v 15'
+    'H264 CPU' = 'libx265 -preset medium -crf 18'
+    'HEVC CPU' = 'libx264 -preset slow -crf 15'
+}
+
+$EncCommands.Keys | ForEach-Object -Begin {
     $script:shouldStop = $false
 } -Process {
     if ($shouldStop -eq $true) { return }
-    Invoke-Expression "ffmpeg.exe -loglevel fatal -f lavfi -i nullsrc=3840x2160 -t 0.1 -c:v $_ -f null NUL"
+    Invoke-Expression "ffmpeg.exe -loglevel fatal -f lavfi -i nullsrc=$Resolution -t 0.1 -c:v $($EncCommands.$_) -f null NUL"
     if ($LASTEXITCODE -eq 0){
-        $script:valid_args = $_
+        $script:valid_args = $EncCommands.$_
+        $script:valid_ezenc = $_
 
         if ($Silent){
-            Write-Host ("Found compatible encoding settings: {0}" -f $script:valid_args.Split(' ')[0].Replace('_', ' ').ToUpper()) -ForegroundColor Green
+            Write-Host ("Found compatible encoding settings using $PSItem`: {0}" -f ($EncCommands.$_)) -ForegroundColor Green
         }
         $shouldStop = $true # Crappy way to stop the loop since most people that'll execute this will technically be parsing the raw URL as a scriptblock
     }
@@ -41,10 +46,14 @@ $DriverVersion = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVers
 if (-Not($script:valid_args)){
     Write-Host "No compatible encoding settings found (should not happen, is FFmpeg installed?)" -ForegroundColor DarkRed
     Get-Command FFmpeg -Ea Ignore
-    Pause
-    exit
+    pause
+    return
 }
 
-return $valid_args
+if ($EzEncArgs){
+    return $script:valid_ezenc
+}else{
+    return $valid_args
+}
 
 }
