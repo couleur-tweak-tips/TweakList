b40c90c0b1d8b4dce1dc1e8b1c169bab8f06c12f
couleurm <couleurm@users.noreply.github.com>
2022-06-14 13:33:31 +0000

    Autobuild

--- Master.ps1
+++ Master.ps1
@@ -1,7 +1,8 @@
 # This file is automatically built at every commit to add up every function to a single file, this makes it simplier to parse (aka download) and execute.
 
-$CommitCount = 121
-$FuncsCount = 47
+using namespace System.Management.Automation # Needed by Invoke-NGENposh
+$CommitCount = 125
+$FuncsCount = 48
 <#
 The MIT License (MIT)
 
@@ -784,18 +785,31 @@ pause
                 
     }else{
 
+        $Scoop = (Get-Command Scoop.ps1).Source | Split-Path | Split-Path
+
+        if (-Not(Test-Path "$Scoop\buckets\main")){
+            if (-Not(Test-Path "$Scoop\apps\git\current\bin\git.exe")){
+                scoop install git
+            }
+            scoop bucket add main
+        }
+
         $Local = ((scoop cat ffmpeg) | ConvertFrom-Json).version
         $Latest = (Invoke-RestMethod https://raw.githubusercontent.com/ScoopInstaller/Main/master/bucket/ffmpeg.json).version
 
         if ($Local -ne $Latest){
             "FFmpeg version installed using scoop is outdated, updating Scoop.."
-            if (-not(Get-Command git -Ea Ignore)){
+            if (-not(Test-Path "$Scoop\apps\git")){
                 scoop install git
             }
             scoop update
         }
 
         scoop install ffmpeg
+        if ($LASTEXITCODE -ne 0){
+            Write-Warning "Failed to install FFmpeg"
+            pause
+        }
     }
 }
 function Install-Scoop {
@@ -829,6 +843,143 @@ function Install-Scoop {
 
     }
 }
+#using namespace System.Management.Automation # this can't be a function but whatever, it doesn't slow down anything
+# Author:	Collin Chaffin
+# License: MIT (https://github.com/CollinChaffin/psNGENposh/blob/master/LICENSE)
+function Invoke-NGENposh {
+<#
+	.SYNOPSIS
+		This Powershell function performs various SYNCHRONOUS ngen functions
+	
+	.DESCRIPTION
+		This Powershell function performs various SYNCHRONOUS ngen functions
+	
+		Since the purpose of this module is to for interactive use,
+		I intentionally did not include any "Queue" options.
+	
+	.PARAMETER All
+		Regenerate cache for all system assemblies
+	
+	.PARAMETER Force
+		Invoke ngen on currently loaded assembles (ensure up to date even if cached)
+	
+	.EXAMPLE
+		To invoke ngen on currently loaded assembles, skipping those already generated:
+
+		PS C:\> Invoke-NGENposh
+	
+	.EXAMPLE	
+		To invoke ngen on currently loaded assembles (ensure up to date even if cached):
+
+		PS C:\> Invoke-NGENposh -Force
+	
+	.EXAMPLE	
+		To invoke ngen to regenerate cache for all system assemblies (*SEE WARNING BELOW**):
+
+		PS C:\> Invoke-NGENposh -All
+	
+	.NOTES
+		 **WARNING: The '-All' switch since the execution is SYNCHRONOUS will
+					take considerable time, and literally regenerate all the
+					global assembly cache.  There should theoretically be no
+					downside to this, but bear in mind other than time (and cpu)
+					that since all the generated cache files are new, any
+					system backups will consider those files as new and may
+					likely cause your next incremental backup to be much larger
+#>
+	param
+	(
+		[switch]$All,
+		[switch]$Force,
+		[switch]$Confirm
+	)
+
+	if (!$Confirm){
+		Write-Host "Press enter to continue and start using NGENPosh, or press CTRL+C to cancel"
+		pause
+	}
+    
+# INTERNAL HELPER
+function Write-InfoInColor
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[String]$Message,
+		[Parameter(Mandatory = $false)]
+		[ValidateNotNullOrEmpty()]
+		[System.ConsoleColor[]]$Background = $Host.UI.RawUI.BackgroundColor,
+		[Parameter(Mandatory = $false)]
+		[ValidateNotNullOrEmpty()]
+		[System.ConsoleColor[]]$Foreground = $Host.UI.RawUI.ForegroundColor,
+		[Switch]$NoNewline
+	)
+	
+	[HostInformationMessage]$outMessage = @{
+		Message			     = $Message
+		ForegroundColor	     = $Foreground
+		BackgroundColor	     = $Background
+		NoNewline		     = $NoNewline
+	}
+	Write-Information $outMessage -InformationAction Continue
+}
+	
+	Write-InfoInColor "`n===================================================================================" -Foreground 'DarkCyan'
+	Write-InfoInColor "                             BEGINNING TO NGEN                                     " -Foreground 'Cyan'
+	Write-InfoInColor "===================================================================================`n" -Foreground 'DarkCyan'
+	
+	Set-Alias ngenpsh (Join-Path ([System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()) ngen.exe) -Force
+	
+	if ($All)
+	{
+		Write-InfoInColor "EXECUTING GLOBAL NGEN`n`n" -Foreground 'Cyan'
+		ngenpsh update /nologo /force
+	}
+	else
+	{
+		Write-InfoInColor "EXECUTING TARGETED NGEN`n`n" -Foreground 'Cyan'
+		
+		[AppDomain]::CurrentDomain.GetAssemblies() |
+		ForEach-Object {
+			if ($_.Location)
+			{
+				$Name = (Split-Path $_.location -leaf)
+				if ((!($Force)) -and [System.Runtime.InteropServices.RuntimeEnvironment]::FromGlobalAccessCache($_))
+				{
+					Write-InfoInColor "[SKIPPED]" -Foreground 'Yellow' -NoNewLine
+					Write-InfoInColor " :: " -Foreground 'White' -NoNewline
+					Write-InfoInColor "[ $Name ]" -Foreground 'Cyan'
+					
+				}
+				else
+				{
+					
+					ngenpsh install $_.location /nologo | ForEach-Object {
+						if ($?)
+						{
+							Write-InfoInColor "[SUCCESS]" -Foreground 'Green' -NoNewLine
+							Write-InfoInColor " :: " -Foreground 'White' -NoNewline
+							Write-InfoInColor "[ $Name ]" -Foreground 'Cyan'
+						}
+						else
+						{
+							Write-InfoInColor "[FAILURE]" -Foreground 'Red' -NoNewLine
+							Write-InfoInColor " :: " -Foreground 'White' -NoNewline
+							Write-InfoInColor "[ $Name ]" -Foreground 'Cyan'
+						}
+					}
+				}
+			}
+		}
+	}
+	Write-InfoInColor "`n===================================================================================" -Foreground 'DarkCyan'
+	Write-InfoInColor "                               COMPLETED NGEN                                      " -Foreground 'Cyan'
+	Write-InfoInColor "===================================================================================`n" -Foreground 'DarkCyan'
+}
+
+
 # https://github.com/chrisseroka/ps-menu
 function Menu {
     param ([array]$menuItems, [switch]$ReturnIndex=$false, [switch]$Multiselect)
