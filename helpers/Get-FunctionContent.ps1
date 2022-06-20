74f6702b80cf7959d77a34358f6da40c6494a61d
Couleur <82747632+couleurm@users.noreply.github.com>
2022-06-20 22:59:22 +0200

    get-functioncontent: add alias support

--- helpers/Get-FunctionContent.ps1
+++ helpers/Get-FunctionContent.ps1
@@ -1,5 +1,9 @@
 function Get-FunctionContent {
     [alias('gfc')]
     param([Parameter()][String]$FunctionName)
+    if ((Get-Command $FunctionName).ResolvedCommand){
+        Write-Verbose "Switching from alias $FunctionName to function $(((Get-Command $FunctionName).ResolvedCommand).Name)"
+        $FunctionName = ((Get-Command $FunctionName).ResolvedCommand).Name
+    }
     return (Get-Command $FunctionName).ScriptBlock
 }
