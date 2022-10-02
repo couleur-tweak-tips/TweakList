0dc87924bcd0416dfb25a8ec081ff0ce7d187a75
Couleur <82747632+couleurm@users.noreply.github.com>
2022-10-02 12:34:28 +0200

    Update TLShell with offline version

--- helpers/Get-TLShell.ps1
+++ helpers/Get-TLShell.ps1
@@ -1,14 +1,40 @@
 function Get-TLShell {
-    param([switch]$Profile)
-
-if ($Profile){
+    param(
+        [switch]$Offline,
+        [switch]$DontOpen
+        )
     
-
-}else{
-
     $WR = "$env:LOCALAPPDATA\Microsoft\WindowsApps" # I've had the habit of calling this folder WR
                                                     # because it's the only folder I know that is added to path
                                                     # that you don't need perms to access.
+
+if ($Offline){
+    
+    try {
+        $Master = Invoke-RestMethod -UseBasicParsing https://raw.githubusercontent.com/couleur-tweak-tips/TweakList/master/Master.ps1
+    } catch {
+        Write-Host "Failed to get Master.ps1 from TweakList GitHub" -ForegroundColor DarkRed
+        Write-Output "Error: $($Error[0].ToString())"
+        return
+    }
+    Set-Content "$WR/TLSOff.cmd" -Value @'
+<# : batch portion
+@echo off
+powershell.exe -noexit -noprofile -noexit -command "iex (${%~f0} | out-string)"
+: end batch / begin powershell #>
+Write-Host "TweakList Shell " -Foregroundcolor White -NoNewLine
+Write-Host "(Offline)" -Foregroundcolor DarkGray -NoNewLine
+Write-Host " - dsc.gg/CTT" -Foregroundcolor White -NoNewLine
+
+'@
+    $Batch = Get-Item  "$WR/TLSOff.cmd"
+    Add-Content $Batch -Value $Master
+    if (!$DontOpen){
+        explorer.exe /select,`"$($Batch.FullName)`"
+    }
+
+}else{
+
 
     
     if ($WR -NotIn $env:PATH.Split(';')){
@@ -24,7 +50,7 @@ if /I "%1" == "so" (set sophiaflag=Write-Host 'Importing Sophia Script..' -NoNew
 
 fltmc >nul 2>&1 || (
     echo Elevating to admin..
-    PowerShell.exe -NoProfile Start-Process -Verb RunAs '%0' 2> nul || (
+    PowerShell.exe -NoProfile Start-Process -Verb RunAs ' %0' 2> nul || (
         echo Failed to elevate to admin, launch CMD as Admin and type in "TL"
         pause & exit 1
     )
@@ -40,18 +66,21 @@ iex(irm tl.ctt.cx);^
 Write-Host \"`rTweakList Shell - dsc.gg/CTT                  `n\" -Foregroundcolor White"
 '@ -Force
     }
-    if (-Not(Test-Path -Path $TLS -PathType Leaf)){
-        $ShortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\TweakList Shell.lnk"
-        $WScriptShell = New-Object -ComObject WScript.Shell
-        $Shortcut = $WScriptShell.CreateShortcut($ShortcutPath)
-        #$Shortcut.Icon = (Get-Command powershell.exe).Source
-        $Shortcut.TargetPath = "$WR\TLS.CMD"
-        $Shortcut.Save()
+    $ShortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\TweakList Shell.lnk"
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WScriptShell.CreateShortcut($ShortcutPath)
+    $Shortcut.Icon = (Get-Command powershell.exe).Source + ",0"
+    $Shortcut.TargetPath = "$WR\TLS.CMD"
+    $Shortcut.Save()
 
-        # Got this from my old list of snippets, originally found this on StackOverflow, forgot link
-        $bytes = [System.IO.File]::ReadAllBytes($ShortCutPath)
-        $bytes[0x15] = $bytes[0x15] -bor 0x20 # Set byte 21 (0x15) bit 6 (0x20) ON
-        [System.IO.File]::WriteAllBytes($ShortcutPath, $bytes)
+    # Got this from my old list of snippets, originally found this on StackOverflow, forgot link
+    $bytes = [System.IO.File]::ReadAllBytes($ShortCutPath)
+    $bytes[0x15] = $bytes[0x15] -bor 0x20 # Set byte 21 (0x15) bit 6 (0x20) ON
+    [System.IO.File]::WriteAllBytes($ShortcutPath, $bytes)
+
+    Write-Host "You can now type 'TLS' in Run (Windows+R) to launch it, or from your start menu"
+    if (!$DontOpen){
+        & explorer.exe /select,`"$("$WR\TLS.CMD")`"
     }
     
     
