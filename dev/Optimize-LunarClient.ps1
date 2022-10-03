752451e25c307fcae58c6c54d7469e634e53d7b9
Couleur <82747632+couleurm@users.noreply.github.com>
2022-10-03 17:27:28 +0200

    Optimize LC: lots of fixes & remove restart
    the restarter I removed is actually quite complicated to implement for what it does (would have to do a Moony equivalent) so whatever, exit and restart the game urself :joy:

--- dev/Optimize-LunarClient.ps1
+++ dev/Optimize-LunarClient.ps1
@@ -13,7 +13,7 @@ function Optimize-LunarClient {
             'lowest',
             'off_van'
             )]
-        [Array]$LazyChunkLoadSpeed,
+        [Array]$LazyChunkLoadSpeed = 'low',
 
 
         [ValidateSet(
@@ -21,42 +21,29 @@ function Optimize-LunarClient {
             'NoCosmetics',
             'MinimalViewBobbing',
             'No16xSaturationOverlay',
-            'HideToggleSprint'
+            'HideToggleSprint',
+            'ToggleSneak',
+            'DisableUHCMods',
+            'FullBright',
+            'CouleursPreset'
             )]
-        [Array]$Settings
-    )
+        [Array]$Settings,
 
-    if ('Performance' -in $Settings){
-        Merge-Hashtables -Original $general $Presets.Performance.general
-        Merge-Hashtables -Original $performance $Presets.Performance.performance
-        Write-Diff -Message "notifications from LC friends getting on (causes massive FPS drop)"
-        Write-Diff -Positivity $True -Message "lazy chunk loading at speed $LCLevel"
-        Write-Diff -Message "ground arrows"
-        Write-Diff -Message "player/mob skulls"
-        Write-Diff -Message "foliage (normal/tall grass)"
+        [Switch]$NoBetaWarning,
+        [Switch]$KeepLCOpen
+    )
+    if (!$NoBetaWarning){
+        Write-Warning "This script may corrupt your Lunar Client profiles, continue at your own risk,`nyou're probably safer if you copy the folder located at $(Convert-Path $HOME\.lunarclient\settings\game)"
+        pause
     }
-    if ('MinimalViewBobbing' -in $Settings){
-        Merge-Hashtables -Original $general -Patch $Presets.MinimalViewBobbing.general
-        Write-Diff -Positivity $True -Message "minimal view bobbing"
-    }
-    if ('HideToggleSprint' -in $Settings){
-        Merge-Hashtables -Original $mods -Patch $Presets.HideToggleSprint.mods
-        Write-Diff -Positivity $False -Term "Hid" -Message "ToggleSprint HUD"
-    }
-    if ('ToggleSneak' -in $Settings){
-        Merge-Hashtables -Original $mods $Presets.ToggleSneak.mods
-        Write-Diff -Positivity $True -Message "ToggleSneak"
-    }
-    if ('DisableUHCMods' -in $Settings){
-        Merge-Hashtables -Original $mods -Patch $Presets.DisableUHCMods.mods
-        Write-Diff -Positivity $False -Term "Disabled" -Message "Waypoints mod"
-        Write-Diff -Positivity $False -Term "Disabled" -Message "DirectionHUD mod"
-        Write-Diff -Positivity $False -Term "Disabled" -Message "Coordinates mod"
-        Write-Diff -Positivity $False -Term "Disabled" -Message "ArmorStatus mod"
-    }
-    if ('FullBright' -in $Settings){
-        Merge-Hashtables -Original $mods -Patch $Presets.FullBright.mods
-        Write-Diff -Term "Added" -Positivity $true -Message "Fullbright (disable shaders before restarting)"
+    if (!$KeepLCOpen){
+        while ((Get-Process -Name java?).MainWindowTitle -Like "Lunar Client*"){
+            Write-Host "You must quit Lunar Client before running these optimizations (LC will overwrite them when it exits)" -ForegroundColor Red
+            pause
+        }
+    }else{
+        Write-Warning "You disabled the script from not running if Lunar Client is running, here be dragons!"
+        Start-Sleep -Milliseconds 500
     }
 
     if (!$LazyChunkLoadSpeed -and ('Performance' -in $Settings)){$LazyChunkLoadSpeed = 'low'}
@@ -68,17 +55,18 @@ function Optimize-LunarClient {
         $Profiles += @{ "$($Profile.DisplayName) ($($Profile.Name))" = $Profile}
     }
 
-    while ($Selection -in $Manager.name,$Manager.DisplayName){
-        Write-Host "Select a profile:"
-        $Selection = Menu @([Array[]]$Profiles.Keys + 'Create a new profile')
-        if ($Selection -in $Manager.name,$Manager.DisplayName){
-            if ($VerbosePreference -eq 'Continue'){
-                Write-Host "Manager:`n`n" -ForegroundColor Red
-                Write-Host ($Manager | ConvertTo-Json)
-            }
-            "A profile with the same name already exists!"
+    Write-Host "Select a profile:"
+    $Selection = Menu @([Array[]]'Create a new profile' + [Array[]]$Profiles.Keys)
+    if ($Selection -in $Manager.name,$Manager.DisplayName){
+        if ($VerbosePreference -eq 'Continue'){
+            Write-Host "Error, Manager:`n`n" -ForegroundColor Red
+            Write-Host ($Manager | ConvertTo-Json)
+            return
+            
         }
+        return "A profile with the same name already exists!"
     }
+
     if ($Selection -eq 'Create a new profile'){
         
         $ProfileName = Read-Host "Enter a name for the new profile"
@@ -88,22 +76,27 @@ function Optimize-LunarClient {
             if (-Not(Test-Path ./$_)){Add-Content ./$_ -Value '{}'} # Empty json file 
         }
         Pop-Location
-        $Manager += [PSCustomObject]@{
+        $Selection = [PSCustomObject]@{
 
             name = $ProfileName
             displayName = $ProfileName
             active = $False
+            default = $False
             iconName = 'crossed-swords'
             server = ''
         }
-        $Selection = $Manager # Overwriting the string "Create a new profile" with the fresh one
-        Set-Content -Path "$LCDirectory\settings\game\profile_manager.json" -Value ($Manager | ConvertTo-Json -Depth 99)
+        $Manager += $Selection # Overwriting the string "Create a new profile" with the fresh one
+        Set-Content -Path "$LCDirectory\settings\game\profile_manager.json" -Value ($Manager | ConvertTo-Json -Compress -Depth 99)
+    }else{
+        $Selection = $Profiles.$Selection
     }
 
     $ProfileDir = "$LCDirectory\settings\game\$($Selection.name)"
-
-    ForEach($file in 'general','mods','performance'){ # Assigns $general, $mods and $performance
-        Set-Variable -Name $file -Value (Get-Content "$ProfileDir\$file.json" -ErrorAction Stop | ConvertFrom-Json)
+    Write-Host "after" -ForegroundColor DarkRed
+    ForEach($file in 'general','mods','performance'){ # Assigns $general, $mods and $performance variables
+        Set-Variable -Scope Global -Name $file -Value (Get-Content "$ProfileDir\$file.json" -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop)
+        Write-Host $file -ForegroundColor Red
+        (Get-Variable -Name $file).Value | ConvertTo-Json
     }
     
     $Presets = @{
@@ -128,21 +121,23 @@ function Optimize-LunarClient {
                 }
             }
         }
+        CouleursPreset = @{
+            mods = @{
+                scoreboard = @{
+                    x = 2 # Moves scoreboard 2 pixels to the right
+                }
+            }
+        }
         Performance = @{
             general = @{
                 friend_online_status_bl	= $false
             }
             performance = @{
-                lazy_chunk_loading	= $LCLlevel
+                lazy_chunk_loading	= $LazyChunkLoadSpeed
                 ground_arrows_bl    = $false
                 stuck_arrows_bl     = $false
                 hide_skulls_bl      = $true
                 hide_foliage_bl     = $true
-            }
-        }
-        MinimalViewBobbing = @{
-            general = @{
-                minimal_viewbobbing_bl = $true
             }
         }
         NoCosmetics = @{
@@ -169,13 +164,26 @@ function Optimize-LunarClient {
                 scale_hat_with_skinlayer_bl  = $false
             }
         }
-        FullBright = @{
+        MinimalViewBobbing = @{
+            general = @{
+                minimal_viewbobbing_bl = $true
+            }
+        }
+        HideToggleSprint = @{
             mods = @{
-                lighting =@{
-                    lighting_enabled_bl = $true
+                toggleSneak = @{
+                     options = @{
+                         showHudText_bl = $false
+                     }
+                }
+            }
+        }
+        ToggleSneak = @{
+            mods = @{
+                toggleSneak = @{
                     options = @{
-                        full_bright_bl	= $true
-                    } 
+                        toggle_sneak_bl = $true
+                    }
                 }
             }
         }
@@ -195,42 +203,86 @@ function Optimize-LunarClient {
                 }
             }
         }
-        ToggleSneak = @{
+        FullBright = @{
             mods = @{
-                toggleSneak = @{
+                lighting = @{
+                    lighting_enabled_bl = $true
                     options = @{
-                        toggle_sneak_bl = $true
-                    }
+                        full_bright_bl	= $true
+                    } 
                 }
             }
         }
-        HideToggleSprint = @{
-            mods = @{
-                toggleSneak = @{
-                     options = @{
-                         showHudText_bl = $false
-                     }
-                }
-            }
-        }
-    }
-    if ($PSVersionTable.$PSVersionTable.$Major -eq 5){
-        $LCProc = Get-Process -Name java? | Where-Object MainWindowTitle -Like "Lunar Client (*/*)"
-        if ($LCProc){
-            "Would you like to restart Lunar Client? [Y/N]"
-            if ((Set-Choice "YN") -eq 'Y'){
-                $mv = "$LCDirectory\offline\multiver"
-                if(Test-Path $mv){$jrewd = $mv}
-                else{
-                    
-                }
-                $CommandLine =  (Get-WmiObject win32_process -filter "ProcessID='$($LCProc.Id)'").CommandLine
-                Stop-Process $LCProc -Force -ErrorAction Stop
-                Push-Location "$LCDirectory\offline\multiver"
-                & $CommandLine
-                Pop-Location
-            }
 
-        }
     }
+        # Whatever you do that's highly recommended :+1:
+    $general = Merge-Hashtables -Original $general -Patch $Presets.All.general
+    $mods = Merge-Hashtables -Original $mods -Patch $Presets.All.mods
+    Write-Diff "Setting recommended settings (compact mods, fast chat).."
+
+    if ('Performance' -in $Settings){
+        $general = Merge-Hashtables -Original $general -Patch $Presets.Performance.general
+        $performance = Merge-Hashtables -Original $performance -Patch $Presets.Performance.performance
+        Write-Diff -Message "notifications from LC friends getting on (causes massive FPS drop)"
+        Write-Diff -Positivity $True -Message "lazy chunk loading at speed $LazyChunkLoadSpeed"
+        Write-Diff -Message "ground arrows"
+        Write-Diff -Message "player/mob skulls"
+        Write-Diff -Message "foliage (normal/tall grass)"
+    }
+    if ('NoCosmetics' -in $Settings){
+        $general = Merge-Hashtables -Original $general -Patch $Presets.NoCosmetics.general
+        ForEach($CosmeticRemoved in @(
+            "cloth cloaks" 
+            "cosmetic particles" 
+            "backpacks" 
+            "pets"
+            "dragon wings"
+            "bandannas"
+            "masks"
+            "belts"
+            "neckwears"
+            "bodywears"
+            "hats"
+            "emotes rendering"
+            "emote particles rendering"
+            "cloaks"
+        )){
+            Write-Diff -Message $CosmeticRemoved -Term "Disabled"
+        }
+
+    }
+    if ('MinimalViewBobbing' -in $Settings){
+        $general = Merge-Hashtables -Original $general -Patch $Presets.MinimalViewBobbing.general
+        Write-Diff -Positivity $True -Message "minimal view bobbing"
+    }
+    if ('HideToggleSprint' -in $Settings){
+        $mods = Merge-Hashtables -Original $mods -Patch $Presets.HideToggleSprint.mods
+        Write-Diff -Positivity $False -Term "Hid" -Message "ToggleSprint HUD"
+    }
+    if ('ToggleSneak' -in $Settings){
+        $mods = Merge-Hashtables -Original $mods $Presets.ToggleSneak.mods
+        Write-Diff -Positivity $True -Message "ToggleSneak"
+    }
+    if ('DisableUHCMods' -in $Settings){
+        $mods = Merge-Hashtables -Original $mods -Patch $Presets.DisableUHCMods.mods
+        Write-Diff -Positivity $False -Term "Disabled" -Message "Waypoints mod"
+        Write-Diff -Positivity $False -Term "Disabled" -Message "DirectionHUD mod"
+        Write-Diff -Positivity $False -Term "Disabled" -Message "Coordinates mod"
+        Write-Diff -Positivity $False -Term "Disabled" -Message "ArmorStatus mod"
+    }
+    if ('FullBright' -in $Settings){
+        $mods = Merge-Hashtables -Original $mods -Patch $Presets.FullBright.mods
+        Write-Diff -Term "Added" -Positivity $true -Message "Fullbright (disable shaders before restarting)"
+    }
+    if ('CouleursPreset' -in $Settings){
+        $mods = Merge-Hashtables -Original $mods -Patch $Presets.CouleursPreset.mods
+    }
+
+    Write-Host "after" -ForegroundColor DarkRed
+    ForEach($file in 'general','mods','performance'){ # Assigns $general, $mods and $performance variables
+        Write-Host $file -ForegroundColor Red
+        (Get-Variable -Name $file).Value
+        ConvertTo-Json -Depth 99 -Compress -InputObject (Get-Variable -Name $file).Value -ErrorAction Stop | Set-Content "$ProfileDir\$file.json" -ErrorAction Stop
+    }
+
 }
