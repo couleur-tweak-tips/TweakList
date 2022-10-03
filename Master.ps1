de9e8a9d160ba25b84f59b79a5dbb71d057aa1dc
couleurm <couleurm@users.noreply.github.com>
2022-10-03 17:34:00 +0000

    Autobuild

--- Master.ps1
+++ Master.ps1
@@ -1,7 +1,7 @@
 # This file is automatically built at every commit to add up every function to a single file, this makes it simplier to parse (aka download) and execute.
 
 using namespace System.Management.Automation # Needed by Invoke-NGENposh
-$CommitCount = 173
+$CommitCount = 175
 $FuncsCount = 55
 <#
 The MIT License (MIT)
