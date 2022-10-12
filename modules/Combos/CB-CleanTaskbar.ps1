e98edb4e4255bb1e0d6e6995c3f8ca436f4f4cb6
Couleur <82747632+couleurm@users.noreply.github.com>
2022-10-12 20:27:33 +0200

    CleanTaskbar: better ipso check, remove met now

--- modules/Combos/CB-CleanTaskbar.ps1
+++ modules/Combos/CB-CleanTaskbar.ps1
@@ -1,8 +1,13 @@
 function CB-CleanTaskbar {
-	Invoke-Expression (Import-Sophia)
+	if (-Not(Get-Module -Name "Sophia Script (TL)" -Ea 0)){
+		Import-Sophia
+	}
 	CortanaButton -Hide
 	PeopleTaskbar -Hide
 	TaskBarSearch -Hide
 	TaskViewButton -Hide
 	UnpinTaskbarShortcuts Edge, Store, Mail
+
+	# Remove "Meet now" from the taskbar, s/o privacy.sexy
+	Set-ItemProperty -Path "Registry::HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAMeetNow" -Value 1
 }
