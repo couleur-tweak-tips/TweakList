3f803c0e220f9625fd21635dd6a8ec99ccd1f6e5
couleurm <couleurm@users.noreply.github.com>
2022-10-27 09:55:10 +0000

    Autobuild

--- Master.ps1
+++ Master.ps1
@@ -1,8 +1,8 @@
 # This file is automatically built at every commit to add up every function to a single file, this makes it simplier to parse (aka download) and execute.
 
 using namespace System.Management.Automation # Needed by Invoke-NGENposh
-$CommitCount = 195
-$FuncsCount = 59
+$CommitCount = 200
+$FuncsCount = 60
 <#
 The MIT License (MIT)
 
@@ -2221,18 +2221,6 @@ function CB-CleanTaskbar {
 	# Remove "Meet now" from the taskbar, s/o privacy.sexy
 	Set-ItemProperty -Path "Registry::HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAMeetNow" -Value 1
 }
-$OPTLC_PRESETS = @(
-    'Performance',
-    'NoCosmetics',
-    'MinimalViewBobbing',
-    'No16xSaturationOverlay',
-    'HideToggleSprint',
-    'ToggleSneak',
-    'DisableUHCMods',
-    'FullBright',
-    'CouleursPreset'
-)
-
 function Optimize-LunarClient {
     [alias('optlc')]
     param(
@@ -2247,8 +2235,29 @@ function Optimize-LunarClient {
         [Array]$LazyChunkLoadSpeed = 'low',
 
 
-        [ValidateSet({$OPTLC_PRESETS})]
-        [Array]$Settings = (Invoke-Checkbox -Title "Select tweaks to apply" -Items $OPTLC_PRESETS),
+        [ValidateSet(
+            'Performance',
+            'NoCosmetics',
+            'MinimalViewBobbing',
+            'No16xSaturationOverlay',
+            'HideToggleSprint',
+            'ToggleSneak',
+            'DisableUHCMods',
+            'FullBright',
+            'CouleursPreset'    
+        )]
+        # Gotta be put twice because mf cant handle variables in validate sets
+        [Array]$Settings = (Invoke-Checkbox -Title "Select tweaks to apply" -Items @(
+            'Performance'
+            'NoCosmetics'
+            'MinimalViewBobbing'
+            'No16xSaturationOverlay'
+            'HideToggleSprint'
+            'ToggleSneak'
+            'DisableUHCMods'
+            'FullBright'
+            'CouleursPreset'
+        )),
        
         [String]
         $LCDirectory = "$HOME\.lunarclient",
@@ -2257,8 +2266,9 @@ function Optimize-LunarClient {
         [Switch]$KeepLCOpen,
         [Switch]$DryRun
 
-        #! [Array]$Misc HideFoliage, NoEntityShadow, LCNametags, Clearglass
+        #! [Array]$Misc HideFoliage, NoEntityShadow, LCNametags, Clearglass, NoBackground, NoHypixelMods
     )
+    
     if (-Not(Test-Path $LCDirectory)){
         Write-Host "Lunar Client's directory ($HOME\.lunarclient) does not exist (for the turbonerds reading this you can overwrite that with -LCDirectory"
     }
@@ -2338,6 +2348,7 @@ function Optimize-LunarClient {
                 compact_menu_bl          = $true
                 modernKeybindHandling_bl = $true
                 borderless_fullscreen_bl = $true
+                trans_res_pack_menu_bg_bl = $true
             }
             mods = @{
                 chat = @{
@@ -2355,7 +2366,35 @@ function Optimize-LunarClient {
         CouleursPreset = @{
             mods = @{
                 scoreboard = @{
+                    seen = $True
                     x = 2 # Moves scoreboard 2 pixels to the right
+                }
+                potioneffects = @{
+                    seen = $True
+                    position = 'bottom_left'
+                    y = -246.5 # Middle left
+                }
+                saturation_hud_mod = @{
+                    seen = $True
+                    saturation_hud_mod_enabled_bl = $True
+                    position     = 'bottom_right' # Just right to the last hunger bar
+                    x            = -289
+                    y            = -37
+                    options = @{
+                        scale_nr          = 1.5 # Yellow
+                        text_clr_nr       = @{value=-171}
+                        background_clr_nr = @{value=0}
+                    }
+                }
+                zoom = @{
+                    seen = $True
+                    options = @{
+                        zoom_kblc         = 'KEY_X'
+                    }
+                }
+                bossbar = @{
+                    seen = $True
+                    bossbar_enabled_bl = $False
                 }
             }
         }
@@ -2398,6 +2437,15 @@ function Optimize-LunarClient {
         MinimalViewBobbing = @{
             general = @{
                 minimal_viewbobbing_bl = $true
+            }
+        }
+        No16xSaturationOverlay = @{
+            mods = @{
+                saturation_mod = @{
+                    options = @{
+                        show_saturation_overlay_bl=$False
+                    }
+                }
             }
         }
         HideToggleSprint = @{
@@ -2486,6 +2534,10 @@ function Optimize-LunarClient {
         $general = Merge-Hashtables -Original $general -Patch $Presets.MinimalViewBobbing.general
         Write-Diff -Positivity $True -Message "minimal view bobbing"
     }
+    if ('No16xSaturationOverlay' -in $Settings){
+        $mods = Merge-Hashtables -Original $mods -Patch $Presets.No16xSaturationOverlay.mods
+        Write-Diff -Positivity $False -Message "16x saturation hunger bar overlay"
+    }
     if ('HideToggleSprint' -in $Settings){
         $mods = Merge-Hashtables -Original $mods -Patch $Presets.HideToggleSprint.mods
         Write-Diff -Positivity $False -Term "Hid" -Message "ToggleSprint HUD"
@@ -2522,7 +2574,6 @@ function Optimize-LunarClient {
 function Optimize-OBS {
     [alias('optobs')]
     param(
-        [Parameter(Mandatory)] # Override encoder check
         [ValidateSet('x264','NVENC','AMF','QuickSync')]
         [String]$Encoder,
         
@@ -2531,6 +2582,18 @@ function Optimize-OBS {
 
         [String]$Preset = 'HighPerformance'
     )
+    Write-Warning "Update your OBS to version 28.0 or above for compatibility with new NVENC/AMD settings"
+    if (!$Encoder){
+        $Encoders = @{
+            "NVENC (NVIDIA GPUs)" = "NVENC"
+            "AMF (AMD GPUs)" = "AMF"
+            "QuickSync (Intel iGPUs)" = "QuickSync"
+            "x264 (CPU)" = "x264"
+        }
+        Write-Host "Select what OBS will use to record (use arrow keys and press ENTER to confirm)"
+        $Key = Menu ([Collections.ArrayList]$Encoders.Keys)
+        $Encoder = $Encoders.$Key # Getting it back from 
+    }
 
     $OBSPatches = @{
         HighPerformance = @{
@@ -2562,7 +2625,8 @@ function Optimize-OBS {
                 recordEncoder = @{
                     'cqp' = 20
                     preset = 'speed'
-                    rate_control = 'cqp'
+                    rate_control = 'CQP'
+                    ffmpeg_opts = "MaxNumRefFrames=4 HighMotionQualityBoostEnable=1"
                 }
             }
             QuickSync = @{
@@ -2764,7 +2828,7 @@ OutputCY=$DefaultHeight
 function Optimize-OptiFine {
     [alias('optof')]
     param(
-        [ValidateSet('Smart','Lowest')]
+        [ValidateSet('Smart','Lowest','CouleursPreset')]
         [Parameter(Mandatory)]
         $Preset,
         [String]$CustomDirectory = (Join-path $env:APPDATA '.minecraft'),
@@ -2796,6 +2860,24 @@ if($MultiMC){
 }
 
 $Presets = @{
+
+    CouleursPresets = @{
+        options = @{
+            'key_key.hotbar.5' = 19
+            'key_key.hotbar.6' = 20
+            'key_key.hotbar.7' = 33
+            'key_key.hotbar.8' = 34
+            'key_key.hotbar.9' = 0
+            chatScale      = 0.8
+            chatWidth      = 0.65
+            guiScale       = 3
+            renderDistance = 7
+            maxFps         = 260
+            chatOpacity    = 0.25
+            enableVsync    = $False
+            pauseOnLostFocus = $False
+        }
+    }
 
     Smart = @{
         options = @{
@@ -2890,7 +2972,9 @@ foreach ($file in 'options','optionsof'){
     $Hash = Merge-Hashtables -Original $Hash -Patch $Presets.$Preset.$file
     Set-Content "$CustomDirectory\$file.txt" -Value (ConvertTo-MCSetting $Hash) -Force
 }
-$Hash = (Get-Content "$CustomDirectory\optionsLC.txt") -Replace ',"maxFps":"260"','' | ConvertFrom-Json
+if (Test-Path "$CustomDirectory\optionsLC.txt"){
+    $Hash = (Get-Content "$CustomDirectory\optionsLC.txt") -Replace ',"maxFps":"260"','' | ConvertFrom-Json
+}
 $Hash = Merge-Hashtables -Original $Hash -Patch $Presets.$Preset.optionsof
 $Hash = Merge-Hashtables -Original $Hash -Patch $Presets.$Preset.options
 $Hash.maxFPS = 260
@@ -4047,6 +4131,144 @@ function Invoke-GitHubScript {
         'Fido'{Invoke-URL https://raw.githubusercontent.com/pbatard/Fido/master/Fido.ps1}
         'SophiaScript'{Import-Sophia}
     }
+}
+function Launch{
+	[alias('l')]
+	param(
+		[ValidateSet(
+			'DisplayDriverUninstaller',
+			'NVCleanstall',
+			'NvidiaProfileInspector',
+			'MSIUtilityV3',
+			'Rufus',
+			'AutoRuns',
+			'Procmon',
+			'CustomResolutionUtility',
+			'NotepadReplacer',
+			'privacy.sexy'
+			#! TODO: NVProfileInspector, MSIUtility, CRU, Notepadreplacer, BulkCrapUninstaller, https://www.bill2-software.com/processmanager/exe/BPM-Setup.exe
+		)]
+		[Array]$Apps,
+		[Switch]$DontLaunch, # Just keep them tidy in the Downloads folder))
+		# This is the non hardcoded Downloads folder path s/o @farag2
+		[String]$OutDir = (Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}")
+	)
+
+	Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+	function Invoke-Download{
+		param(
+			[String]$URL, # Parses mediafire
+			[String]$AppName,
+			[Switch]$Scoop, # Scoop 'bucket/manifest' name
+			[String]$PathToBinary, # In the zip
+			[String]$Checksum,
+			[String]$SelfExtracting7z # e.g for DDU
+		)
+
+		if (-Not(Test-Path $env:TMP)){
+			throw "TMP environment variable not found [$env:TMP]"
+		}
+
+		if($Scoop){
+			$Bucket, $Manifest = $URL -split '/'
+
+			$Repos = @{
+
+				main = @{org = 'ScoopInstaller';repo = 'main';branch = 'master'}
+				extras = @{org = 'ScoopInstaller';repo = 'extras';branch = 'master'}
+				utils = @{org = 'couleur-tweak-tips';repo = 'utils';branch = 'main'}
+				nirsoft = @{org = 'kodybrown';repo = 'scoop-nirsoft';branch = 'master'}
+				games = @{org = 'ScoopInstaller';repo = 'games';branch = 'master'}
+				'nerd-fonts' = @{org = 'ScoopInstaller';repo = 'nerd-fonts';branch = 'master'}
+				versions = @{org = 'ScoopInstaller';repo = 'versions';branch = 'master'}
+				java = @{org = 'ScoopInstaller';repo = 'java';branch = 'master'}
+			}
+			$repo = $Repos.$Bucket
+			$URL = "https://raw.githubusercontent.com/$($repo.org)/$($repo.repo)/$($repo.branch)/bucket/$Manifest.json"
+			$URL, $Version = Invoke-RestMethod $URL | ForEach-Object {$PSItem.URL, $PSItem.Version}
+		}elseif($URL -Like "*mediafire.com*"){
+			$URL = (Invoke-WebRequest -UseBasicParsing $URL).Links.href | Where-Object {$PSItem -Like "http*://download*.mediafire.com/*"}
+		}
+
+		if ($AppName){
+			$FileName = $AppName
+		}else{
+			$FileName = $Manifest
+		}
+		
+		if ($Version){$FileName += " $Version"}
+
+		$Extension = [io.path]::GetExtension((($URL -replace '#/dl.7z') | Split-Path -Leaf))
+
+		$OutFile = "$env:TMP\$FileName$Extension"
+		if (-Not(Test-Path $OutFile)){
+			curl.exe -#L -A "Scoop" $URL -o"$OutFile"
+		}
+
+		if($Checksum){
+			$Parameters = @{
+				Path = $OutFile
+			}
+			if ($Checksum -Like "*:*"){ # Contains a :
+				$Algo, $Checksum = $Checksum -Split ':' # To split hash and algo, eg md5:8424509737CEDBDE4BA9E9A780D5CE96
+				$Parameters += @{
+					Algorithm = $Algo 
+				}
+			}
+			if ($Checksum -ne (Get-FileHash @Parameters).Hash){
+				throw "Hash provided $Checksum does not match $OutFile"
+			}
+		}
+
+		if ($Extension -eq '.zip'){
+			$OutDir = "$env:TMP\$FileName\"
+			if (-Not(Test-Path $OutDir)){
+				[System.IO.Compression.ZipFile]::ExtractToDirectory($OutFile, $OutDir)
+			}
+
+			if ($PathToBinary){
+				$OutDir = Join-Path $OutDir $PathToBinary
+			}
+			$OutFile = $OutDir # To not have to check for the following statement twice
+		}elseif($SelfExtracting7z){
+			Start-Process -FilePath $OutFile -ArgumentList "-y" -Wait
+			$SelfExtracting7z = $SelfExtracting7z -replace "%VER%", $Version
+			if (-Not(Test-Path "$env:TMP\$SelfExtracting7z" -PathType Container)){
+				throw "Self extracting 7-Zip got wrong path: $SelfExtracting7z"
+			}
+			$OutDir = $SelfExtracting7z
+		}
+
+		if (-Not(Test-Path $OutFile)){
+			throw "$OutFile could not be found"
+		}
+
+		return $OutFile
+
+	}
+
+	$Paths = @()
+
+	$Apps | ForEach-Object { # Cycles through given apps
+		Write-Host "getting $PSItem"
+		$Paths += switch ($PSItem){
+			DisplayDriverUninstaller{ Invoke-Download -URL extras/ddu -Scoop -PathToBinary "Display Driver Uninstaller.exe" -SelfExtracting7z "DDU v%VER%" -AppName DDU }
+			NVCleanstall{ Invoke-Download -URL extras/nvcleanstall -Scoop -AppName NVCleanstall -PathToBinary "NVCleanstall.exe" }
+			NvidiaProfileInspector{ Invoke-Download -URL extras/nvidia-profile-inspector -Scoop -AppName NvidiaProfileInspector -PathToBinary 'nvidiaProfileInspector.exe' }
+			MSIUtilityV3{
+				Write-Warning "MSI mode is already applied by default on NVIDIA 1600/2000/3000 GPUs and AMD cards"
+				Invoke-Download -URL https://www.mediafire.com/file/ewpy1p0rr132thk/MSI_util_v3.zip/file -AppName "MSIUtilV3" -PathToBinary "MSI_util_v3.exe" -Checksum "md5:8424509737CEDBDE4BA9E9A780D5CE96"
+			}
+			Rufus{ Invoke-Download -URL extras/rufus -Scoop -AppName rufus}
+			AutoRuns{ Invoke-Download -URL https://download.sysinternals.com/files/Autoruns.zip -AppName AutoRuns -PathToBinary Autoruns64.exe }
+			Procmon{ Invoke-Download -URL https://download.sysinternals.com/files/ProcessMonitor.zip -AppName Procmon -PathToBinary Procmon64.exe }
+			CustomResolutionUtility { Invoke-Download -URL extras/cru -Scoop -AppName CRU -PathToBinary CRU.exe}
+			NotepadReplacer { Invoke-Download -URL utils/notepadreplacer -Scoop -AppName NotepadReplacer}
+			privacy.sexy { Invoke-Download -URL utils/privacysexy -Scoop -AppName privacysexy}
+		}
+	}
+	return $Paths
 }
 <#!TODO:
     Scan windows defender
