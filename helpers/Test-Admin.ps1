4e948b660d5fad08feeaac71bda3a6cd195b4c3d
Couleur <82747632+couleurm@users.noreply.github.com>
2022-11-20 10:39:07 +0100

    Add *nix support

--- helpers/Test-Admin.ps1
+++ helpers/Test-Admin.ps1
@@ -1,10 +1,12 @@
 function Test-Admin {
-<#
-.SYNOPSIS
-Determines if the console is elevated
 
-#>
-    $identity  = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $principal = New-Object System.Security.Principal.WindowsPrincipal( $identity )
-    return $principal.IsInRole( [System.Security.Principal.WindowsBuiltInRole]::Administrator )
+    if (!$IsLinux -and !$IsMacOS){
+
+        $identity  = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+        $principal = New-Object System.Security.Principal.WindowsPrincipal( $identity )
+        return $principal.IsInRole( [System.Security.Principal.WindowsBuiltInRole]::Administrator )
+    
+    }else{ # Running on *nix
+        return ((id -u) -eq 0)
+    }
 }
