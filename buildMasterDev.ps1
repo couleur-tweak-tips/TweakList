c9c9ae5c2b595b02aac65b38581d43067e6b5b2b
Couleur <82747632+couleurm@users.noreply.github.com>
2022-11-23 18:50:27 +0100

    add temporary MasterDev builder

---
+++ buildMasterDev.ps1
@@ -0,0 +1,42 @@
+[CmdletBinding()]
+param(
+    [Switch]$Import
+)
+
+if ($env:COULEUR){
+    Push-Location "D:\GitHub\TweakList"
+}
+
+$Members = [System.Collections.ArrayList]@()
+$Content = [System.Collections.ArrayList]@(@"
+using namespace System.Management.Automation
+New-Module -Name "TweakList" -ScriptBlock ([ScriptBlock]::Create({
+#region Master`n
+"@)
+
+Get-ChildItem modules, helpers -Recurse | ForEach-Object {
+
+    if ($_.Extension -eq '.ps1'){
+        try {
+            . $_.FullName -ErrorAction Stop
+            Get-Command $_.BaseName -CommandType Function -ErrorAction Stop | Out-Null
+        } catch {
+            $_
+            throw
+        }
+        $Content += (Get-Content $_.FullName -Raw -Verbose) #, "Export-ModuleMember $($_.BaseName) -Alias *"
+        $Members.Add($_.BaseName) | Out-Null
+    }
+}
+$Content += @"
+`n#endregion
+Export-ModuleMember $($Members -join ", ") -Alias *
+})) | Import-Module -DisableNameChecking -Global
+"@
+if ($Import){
+    & ([ScriptBlock]::Create($Content))
+}else{
+    Set-Content -Value $Content -Path ./MasterDev.ps1
+}
+
+Pop-Location
