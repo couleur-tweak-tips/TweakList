b923620f6365ca81a072ec4288952efb79e5c12a
Couleur <82747632+couleurm@users.noreply.github.com>
2022-11-23 18:58:02 +0100

    Set-PowerPlan: fix URL argument

--- modules/Windows/Set-PowerPlan.ps1
+++ modules/Windows/Set-PowerPlan.ps1
@@ -7,8 +7,12 @@ function Set-PowerPlan {
     if ($Ultimate){
         powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
         powercfg /setactive e9a42b02-d5df-448d-aa00-03f14749eb61
-    }elseif($PowURL){
-        $DotPow = "$env:TMP\Powerplan $(Get-Random).pow"
+    }elseif($URL){
+        if ($URL -Like "http*://cdn.discordapp.com/attachments/*.pow"){
+            $DotPow = "$env:TMP\{0}" -f (Split-Path $URL -Leaf)
+        }else{
+            $DotPow = "$env:TMP\Powerplan $(Get-Random).pow"
+        }
         Invoke-WebRequest -Uri $PowURL -OutFile $DotPow
         powercfg -duplicatescheme $DotPow
         powercfg /s $DotPow
