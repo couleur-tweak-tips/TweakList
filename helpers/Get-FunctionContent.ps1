7c404169fe9301cd1259e93a9fafbc658d02a0ea
Couleur <82747632+couleurm@users.noreply.github.com>
2022-11-23 18:51:00 +0100

    gfc: can now return a tweakfuncs' dependencies

--- helpers/Get-FunctionContent.ps1
+++ helpers/Get-FunctionContent.ps1
@@ -1,9 +1,66 @@
 function Get-FunctionContent {
     [alias('gfc')]
-    param([Parameter()][String]$FunctionName)
-    if ((Get-Command $FunctionName).ResolvedCommand){
-        Write-Verbose "Switching from alias $FunctionName to function $(((Get-Command $FunctionName).ResolvedCommand).Name)"
-        $FunctionName = ((Get-Command $FunctionName).ResolvedCommand).Name
+    [CmdletBinding()]
+    param([Parameter()]
+        [String[]]$Functions,
+        [Switch]$Dependencies,
+        [Switch]$ReturnNames
+    )
+
+    $FunctionsPassed = [System.Collections.ArrayList]@()
+    $Content = [System.Collections.ArrayList]@()
+
+    Get-Command $Functions -ErrorAction Stop | ForEach-Object { # Loop through all functions
+        if ($Resolved = $_.ResolvedCommand){ # Checks if $_.ResolveCommand exists, also assigns it to $Resolved
+            Write-Verbose "Switching from alias $_ to function $($Resolved.Name)" -Verbose
+            $_ = Get-Command $Resolved.Name
+        }
+        if ($_ -NotIn $FunctionsPassed){ # If it hasn't been looped over yet
+
+            $Content += ($Block = $_.ScriptBlock.Ast.Extent.Text)
+                # Assigns function block to $Block and appends to $Content
+            
+            $FunctionsPassed.Add($_) | Out-Null # So it doesn't get checked again
+
+            if ($Dependencies){
+
+                if (!$TL_FUNCTIONS){
+                    if (Get-Module -Name TweakList -ErrorAction Ignore){
+                        $TL_FUNCTIONS = [String[]](Get-Module -Name TweakList).ExportedFunctions.Keys
+                    }else {
+                        throw "TL_FUNCTIONS variable is not defined, which is needed to get available TweakList functions"
+                    }
+                }
+
+                $AST = [System.Management.Automation.Language.Parser]::ParseInput($Block, [ref]$null, [ref]$null)
+                
+                $DepMatches = $AST.FindAll({
+                        param ($node)
+                        $node.GetType().Name -eq 'CommandAst'
+                    }, $true) | #It gets all cmdlets from the Abstract Syntax Tree
+                ForEach-Object {$_.CommandElements[0].Value} | # Returns their name
+                    Where-Object { # Filters out only TweakList functions
+                        $_ -In ($TL_FUNCTIONS | Where-Object {$_ -ne $_.Name})
+
+                    } | Select-Object -Unique
+
+                ForEach($Match in $DepMatches){
+                    $FunctionsPassed.Add((Get-Command -Name $Match -CommandType Function)) | Out-Null
+
+                    $Content += (Get-Command -Name $Match -CommandType Function).ScriptBlock.Ast.Extent.Text
+
+                }
+            }
+        }
     }
-    return (Get-Command $FunctionName).ScriptBlock
+
+    if ($Content){
+        $Content = "#region gfc`n" + $Content + "`n#endregion"
+    }
+
+    if($ReturnNames){
+        return $FunctionsPassed | Select-Object -Unique # | Where-Object {$_ -notin $Functions} 
+    } else {
+        return $Content
+    }
 }
