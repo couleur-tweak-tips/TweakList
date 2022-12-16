3f1da8427f015328982b54ed28deb83c9a9b8e8f
couleurm <couleurm@users.noreply.github.com>
2022-12-16 10:17:39 +0000

    Autobuild

--- Master.ps1
+++ Master.ps1
@@ -1,7 +1,7 @@
 # This file is automatically built at every commit to add up every function to a single file, this makes it simplier to parse (aka download) and execute.
 
 using namespace System.Management.Automation # Needed by Invoke-NGENposh
-$CommitCount = 282
+$CommitCount = 284
 $FuncsCount = 65
 function Get-IniContent {
     <#
