152566ca3cd8abe1f04377bbb4a8a0320d836d12
Couleur <82747632+couleurm@users.noreply.github.com>
2023-01-28 13:59:44 +0100

    add Install-ZetaLoader

---
+++ modules/Installers/Install-ZetaLoader.ps1
@@ -0,0 +1,11 @@
+function Install-ZetaLoader {
+    $GameInstallDir = Get-SteamGameInstallDir "Halo Infinite"
+    $ZetaLoader = "$((Invoke-RestMethod "https://api.github.com/repos/Aetopia/ZetaLoader/releases/latest").assets[0].browser_download_url)"
+    if (!$GameInstallDir) {
+        Write-Error "Halo Infinite hasn't been installed via Steam."
+        exit 1
+    }
+    Write-Output "Installing ZetaLoader..."
+    Invoke-RestMethod -Uri "$ZetaLoader" -OutFile "$GameInstallDir\DumpTool.exe"
+    Write-Output "ZetaLoader has been installed."
+}
